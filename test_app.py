a983e983a39945b3976990c5729c103208de0981
PSN <psn992273@gmail.com>
2025-04-10 03:40:54 +0000

    Commit6

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_index():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello from Jenkins" in response.data
+    assert b"Hello from Jenkins1" in response.data
